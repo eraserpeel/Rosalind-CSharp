62fc1d8e2923ca423d08f1ab2fc057ce5052e6d5
slogmore <eraserpeel@gmail.com>
2015-08-03 20:42:22 -0600

    Finished a few more

---
+++ Rosalind Challenges/7 - Mendels First Law.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rosalind_Challenges
+{
+    class MendelsFirstLaw
+    {
+
+        double CalculateProbability(double k, double m, double n)
+        {
+            double numCreatures = k + m + n;
+            double probOfDominant = 0;
+
+            if (k > 1)
+                probOfDominant = probOfDominant + (k / numCreatures);
+            
+            if (m > 1)
+                probOfDominant = probOfDominant + ((0.75) * ((m / numCreatures) * ((m - 1) / (numCreatures - 1))));
+            
+            probOfDominant = probOfDominant + ((m / numCreatures) * ((k) / (numCreatures - 1)));
+            probOfDominant = probOfDominant + ((0.50) * ((m / numCreatures) * ((n) / (numCreatures - 1))));
+            probOfDominant = probOfDominant + ((0.50) * ((n / numCreatures) * ((m) / (numCreatures - 1))));
+            probOfDominant = probOfDominant + ((n / numCreatures) * ((k) / (numCreatures - 1)));
+            
+            return probOfDominant;
+        }
+
+        static void Main(string[] args)
+        {
+            MendelsFirstLaw mf = new MendelsFirstLaw();
+            Console.WriteLine(mf.CalculateProbability(15, 23, 26));
+            Console.Read();
+        }
+
+    }
+}
---
+++ Rosalind Challenges/8 - Translating RNA to Protein.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rosalind_Challenges
+{
+    class TranslatingRNA
+    {
+        Dictionary<string, string> codonLookup;
+
+        public TranslatingRNA() 
+        {
+            codonLookup = new Dictionary<string,string>();
+            LoadLookupTable();
+        }
+
+        void LoadLookupTable()
+        {
+            string text = LoadFile("../../codon_table.txt");
+            string [] splitText = text.Replace(Environment.NewLine, " ").Split(' ');
+            splitText = splitText.Where(c => c.Length > 0).ToArray();
+            for (int i = 0; i < splitText.Length - 1; i = i + 2)
+                codonLookup.Add(splitText[i], splitText[i + 1]);
+            
+        }
+
+        string LoadFile(string filename)
+        {
+            try
+            {
+                return System.IO.File.ReadAllText(filename);
+            }
+            catch (FileNotFoundException ex)
+            {
+                return ex.ToString();
+            }
+        }
+
+        string Translate(string rna)
+        {
+            string translation = "";
+            for (int i = 0; i < rna.Length; i = i + 3)
+                translation = translation + codonLookup[rna.Substring(i, 3)];
+
+            return translation;
+        }
+
+        static void Main(string[] args)
+        {
+            TranslatingRNA tr = new TranslatingRNA();
+            Console.WriteLine(tr.Translate(tr.LoadFile("../../8-data.txt")));
+            Console.Read();
+        }
+    }
+}
